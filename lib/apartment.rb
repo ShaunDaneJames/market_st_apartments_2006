224d9ad081d1e064ad8335ab9543447fb114031d
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-14 09:20:52 -0600

    Passes room number

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,6 +1,6 @@
 class Apartment
   attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
-  def initialize(apartment_info)
+  def initialize(number: '', monthly_rent: 0, bathrooms: 0, bedrooms: 0)
     @number = number
     @monthly_rent = monthly_rent
     @bathrooms = bathrooms
