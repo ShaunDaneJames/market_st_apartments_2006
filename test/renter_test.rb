8a99329c4cd2ed707efe8e51f0ce41ada1e036ab
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-14 09:32:36 -0600

    changes were made, mistakes as well

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -5,12 +5,12 @@ require './lib/renter'
 class RenterTest < Minitest::Test
 
   def test_it_exists
-    renter = Renter.new
+    renter = Renter.new("Jessie")
     assert_instance_of Renter, renter
   end
 
   def test_renters_have_names
     renter = Renter.new("Jessie")
     assert_equal "Jessie", renter.name
-  end 
+  end
 end
