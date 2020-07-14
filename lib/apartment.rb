a0eaa17f21770af626c87cbcbb987e9392fd3e6c
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-14 09:12:16 -0600

    Passes it exists

---
+++ lib/apartment.rb
@@ -0,0 +1,10 @@
+class Apartment
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  def initialize(apartment_info)
+    @number = number
+    @monthly_rent = monthly_rent
+    @bathrooms = bathrooms
+    @bedrooms = bedrooms
+  end
+
+end
