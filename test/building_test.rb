d1459741cab07cfbdc0caf06eaa7940d87deae62
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-14 10:35:06 -0600

    random commit as I have become so lost and distressed

--- test/building_test.rb
+++ test/building_test.rb
@@ -14,5 +14,47 @@ class BuildingTest < Minitest::Test
     building = Building.new
 
     assert_equal [], building.units
-  end 
+  end
+
+  def test_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+
+    assert_equal [unit1, unit2], building.units
+  end
+
+  def test_it_has_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Aurora")
+
+    building.add_unit(unit1)
+    assert_equal [], building.renters
+  end
+
+  def test_it_can_add_renter
+    # skip
+    building = Building.new
+    renter1 = Renter.new("Aurora")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    building.add_unit(unit1)
+    unit1.add_renter(renter1)
+    assert_equal renter1, building.renters
+  end
+
+  def test_avaerage_rent
+    skip
+    building = Building.new
+    renter1 = Renter.new("Aurora")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    assert_equal 1099.5, building.average_rent
+  end
 end
