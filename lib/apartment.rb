718671c84316485b56ad01c6c24d34cf8d619ee6
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-14 09:32:05 -0600

    Passes test for adding renter

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,10 +1,14 @@
 class Apartment
-  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
   def initialize(number: '', monthly_rent: 0, bathrooms: 0, bedrooms: 0)
     @number = number
     @monthly_rent = monthly_rent
     @bathrooms = bathrooms
     @bedrooms = bedrooms
+    @renter = renter
   end
 
+  def add_renter(renter)
+    @renter = renter
+  end 
 end
